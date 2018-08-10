63210f7866f28010608d5f1a1017c613bc9a2cb9
childCoding <pnclt@vip.qq.com>
2018-08-10 18:51:33 +0800

    补交移动消息文件

---
+++ Unity/Hotfix/Module/FrameSync/Frame_UnitMoveHandler.cs
@@ -0,0 +1,19 @@
+﻿using ETModel;
+using UnityEngine;
+
+namespace ETHotfix
+{
+    [MessageHandler]
+    public class Frame_UnitMoveHandler : AMHandler<Frame_UnitMove>
+    {
+        protected override void Run(ETModel.Session session, Frame_UnitMove message)
+        {
+            Unit unit = ETModel.Game.Scene.GetComponent<UnitComponent>().Get(message.Id);
+            MoveComponent moveComponent = unit.GetComponent<MoveComponent>();
+            Vector3 dir = ETModel.Utility.UnityVector3FromETVector3(message.Dir);
+            Vector3 dest = unit.Position + dir * 5;
+            moveComponent.Turn2D(dir);
+            moveComponent.MoveToDest(dest, 1);
+        }
+    }
+}
---
+++ Unity/Hotfix/Module/FrameSync/Frame_UnitMoveStopHandler.cs
@@ -0,0 +1,19 @@
+﻿using ETModel;
+using UnityEngine;
+
+namespace ETHotfix
+{
+    [MessageHandler]
+    public class Frame_UnitMoveStopHandler : AMHandler<Frame_UnitMoveStop>
+    {
+        protected override void Run(ETModel.Session session, Frame_UnitMoveStop message)
+        {
+            Unit unit = ETModel.Game.Scene.GetComponent<UnitComponent>().Get(message.Id);
+            MoveComponent moveComponent = unit.GetComponent<MoveComponent>();
+            //Vector3 dir = ETModel.Utility.UnityVector3FromETVector3(message.Dir);
+            //Vector3 dest = unit.Position + dir * 5;
+            moveComponent.Stop();
+            //moveComponent.MoveToDest(dest, 1);
+        }
+    }
+}
