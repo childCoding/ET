1085c3e76271ade34dae850a466fc0dfa30bdffb
wanderRun <bbyjbb@sohu.com>
2018-08-11 18:50:04 +0800

    ui

--- Unity/Assets/Scripts/UI/Joysticks/FixedJoystick.cs
+++ Unity/Assets/Scripts/UI/Joysticks/FixedJoystick.cs
@@ -20,14 +20,9 @@ public class FixedJoystick : Joystick
     {
         Vector2 direction = eventData.position - joystickPosition;
         inputVector = (direction.magnitude > background.sizeDelta.x / 2f) ? direction.normalized : direction / (background.sizeDelta.x / 2f);
-        
         ClampJoystick();
         handle.anchoredPosition = (inputVector * background.sizeDelta.x / 2f) * handleLimit;
-        //Unit unit = UnitComponent.Instance.MyUnit;
-        //Vector3 moveVector = (unit.Position + Vector3.right * this.Horizontal + Vector3.forward * this.Vertical) * 1000;
-        //SessionComponent.Instance.Session.Send(new Frame_ClickMap() { X = (int)moveVector.x, Z = (int)moveVector.z });
         WorldDirection = mainCamera.cameraToWorldMatrix.MultiplyVector(new Vector3(inputVector.x, 0, -inputVector.y));
-        //Log.Debug($"{WorldDirection} {mainCamera.transform.forward}");
     }
 
     public override void OnPointerDown(PointerEventData eventData)
@@ -40,9 +35,6 @@ public class FixedJoystick : Joystick
         WorldDirection = Vector3.zero;
         inputVector = Vector2.zero;
         handle.anchoredPosition = Vector2.zero;
-        //Unit unit = Game.Scene.GetComponent<UnitComponent>().Get(UnitComponent.Instance.MyUnit.Id);
-        //Vector3 moveVector = unit.Position * 1000;
-        //SessionComponent.Instance.Session.Send(new Frame_ClickMap() { X = (int)moveVector.x, Z = (int)moveVector.z });
     }
     public void Update()
     {
