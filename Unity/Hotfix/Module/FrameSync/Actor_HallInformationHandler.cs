28006b6f26bf244fbbe800a79b9bcfef1400d0d2
wanderRun <bbyjbb@sohu.com>
2018-08-11 11:13:06 +0800

    大厅

---
+++ Unity/Hotfix/Module/FrameSync/Actor_HallInformationHandler.cs
@@ -0,0 +1,20 @@
+﻿using ETModel;
+using UnityEngine;
+
+namespace ETHotfix
+{
+	[MessageHandler]
+	public class Actor_HallInformationHandler : AMHandler<Actor_HallInformation>
+	{
+		protected override void Run(ETModel.Session session, Actor_HallInformation message)
+		{
+            UILobbyComponent uILobbyComponent = Game.Scene.GetComponent<UIComponent>().Get(UIType.UILobby).GetComponent<UILobbyComponent>();
+            string text = "";
+            foreach (PlayerChooseType playerChooseType in message.PlayerChooseType)
+            {
+                text += "(" + playerChooseType.Id.ToString() + ","+ playerChooseType.Type.ToString() + ")";
+            }
+            uILobbyComponent.Text.text = text;
+        }
+	}
+}
